d9448aff36da2f15efc4bc99e6de3d95a394fb46
StartPlatinum_199701-at-715158845633 <wangtianhao810@gmail.com>
2024-09-14 14:04:11 +0100

    clean up init

--- pysrc/__init__.py
+++ pysrc/__init__.py
@@ -58,27 +58,12 @@ class Scrawler:
         return list(set(html_list))
 
 
-# TODO
-def f(url):
-    header = {"Connection": "keep-alive",
-              "Cookie": "qgqp_b_id=5c787bb3c7f4b8443f3a208d36f1e054; qRecords=%5B%7B%22name%22%3A%22%u76DF%u79D1%u836F%u4E1A-U%22%2C%22code%22%3A%22SH688373%22%7D%2C%7B%22name%22%3A%22%u8302%u4E1A%u5546%u4E1A%22%2C%22code%22%3A%22SH600828%22%7D%5D; em-quote-version=topspeed; HAList=ty-0-002335-%u79D1%u534E%u6570%u636E%2Cty-1-605117-%u5FB7%u4E1A%u80A1%u4EFD%2Cty-0-000629-%u9492%u949B%u80A1%u4EFD%2Cty-0-300827-%u4E0A%u80FD%u7535%u6C14%2Cty-1-603063-%u79BE%u671B%u7535%u6C14%2Cty-0-002466-%u5929%u9F50%u9502%u4E1A%2Cty-0-002465-%u6D77%u683C%u901A%u4FE1%2Cty-1-688008-%u6F9C%u8D77%u79D1%u6280%2Ca-sh-688256-%u5BD2%u6B66%u7EAA-U%2Cty-1-603019-%u4E2D%u79D1%u66D9%u5149",
-              "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
-              "Host": "data.eastmoney.com"
-              }
-    response = requests.get(url, headers=header)
-    html = response.content.decode("utf-8")
-    # with open("main.json", "w") as file:
-    #     json.dump(html, file, indent=6)
-    main_text = BeautifulSoup(html, "html.parser").body.find('div', attrs={'class': 'ctx-box'}).text
-
-    return main_text
-
-
 if __name__ == "__main__":
     os.chdir("..")
     dir_path = "data/raw/"
+    to_path = "data/erd/个股研报.csv"
     is_download = True
-    df = pd.read_csv("个股研报.csv")
+    df = pd.read_csv(to_path)
 
     if is_download:
         for report_type in ID_MAP:
@@ -107,4 +92,4 @@ if __name__ == "__main__":
         df["link"] = "https://data.eastmoney.com/report/zw_industry.jshtml?encodeUrl=" + df["encodeUrl"]
         # df["main_text"] = df["link"].apply(f)
 
-        df.to_csv("个股研报.csv", index=False)
+        df.to_csv(to_path, index=False)
